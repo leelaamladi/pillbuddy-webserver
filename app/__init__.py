ae8e4aeecd26e36ebb2db5ee62fcb091eb6a4f85
leelaamladi <leelaamladi@gmail.com>
2014-01-07 00:27:10 -0800

    turning on debug mode on heroku temporarily

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ bcrypt = Bcrypt(app)
 # if running from Heroku, grab config from environment variables
 # else, load from settings.py file
 if bool(os.environ.get('HEROKU')):
-	app.config['DEBUG'] = False
+	app.config['DEBUG'] = True
 	app.config['PORT'] = int(os.environ.get("PORT", 5000))
 	app.config['DB'] = str(os.environ.get('DB_NAME', None))
 	app.config['DB_USERNAME'] = str(os.environ.get('DB_USERNAME', None))
